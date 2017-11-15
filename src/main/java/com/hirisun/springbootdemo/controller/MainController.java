4f3b4e3cb176cf357149ad91318420bdc2a057cb
zhoufeng <zf_fz501087@163.com>
2017-11-15 16:00:33 +0800

    添加jpa,完成spring security 登录验证。添加logout

--- src/main/java/com/hirisun/springbootdemo/controller/MainController.java
+++ src/main/java/com/hirisun/springbootdemo/controller/MainController.java
@@ -2,9 +2,15 @@ package com.hirisun.springbootdemo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class MainController {
@@ -21,5 +27,15 @@ public class MainController {
         LOGGER.debug("This is Login Method!");
         return "login";
 
+    }
+
+    @RequestMapping(value = "/logout",method = RequestMethod.GET)
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        LOGGER.debug("This is Logout Method!");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:login";
     }
 }
